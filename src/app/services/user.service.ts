d4757fcb48ea180b3cc8ba5c993d37626215e65e
Dániel Kludák <kludak.daniel15@gmail.com>
2021-09-28 18:13:12 +0200

    custom header implemented for userservice POST method, CORS policy error fixed

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {UserModel} from "../models/user.model";
 
 const loginUrl = 'https://developer.webstar.hu/rest/frontend-felveteli/authentication/';
@@ -14,7 +14,11 @@ export class UserService {
   }
 
   login(user: UserModel) {
-    return this.http.post(loginUrl, user);
+    //console.log(user);
+    let headers = new HttpHeaders({
+      'Applicant-id': '72fPqjKaG3ea2SR9'});
+    let options = { headers: headers };
+    return this.http.post(loginUrl, user, options);
   }
 
 }
